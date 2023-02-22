788dc56636f4379b50060f6040d67a0ce2bb93ed
Rediskaautofarm <110743861+Rediskaautofarm@users.noreply.github.com>
2023-02-22 12:25:42 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,23 @@
+import os
+try:
+	from aiogram import Bot, Dispatcher, executor, types
+	from aiogram.dispatcher import FSMContext, filters
+	from aiogram.types import Message, CallbackQuery, ChatType
+except:
+	os.system("pip install aiogram")
+
+TOKEN_Telegram = "5979393392:AAF_1lINfvaAO7GiVvMe8AjyU-xJ3uRh_IU"
+client = Bot(TOKEN_Telegram, parse_mode="HTML")
+dp = Dispatcher(client)
+
+@dp.message_handler(content_types=['text'])
+async def main(message: types.Message):
+	content = str(message.text.lower()).split(" ")
+
+	if content[0] == "!ping":
+		await message.answer("test")
+
+
+if __name__ == '__main__':
+	print('работает')
+	executor.start_polling(dp)
